396efe4b7404a20a7b6c1847ded2ec4a5dd5f25d
fadhilrazzan <54299052+fadhilrazzan@users.noreply.github.com>
2019-10-08 20:59:04 +0700

    Add files via upload

---
+++ TUGAS PBO/Aritmatika.java
@@ -0,0 +1,16 @@
+
+package pbo;
+
+public class Aritmatika {
+    public static void main(String[] args){
+        int bil_a, bil_b, hasil;
+        
+        bil_a = 3243;
+        bil_b = 4478;
+        hasil = bil_a + bil_b;
+        System.out.print("Hasil jumlah kedua bilangan : ");
+        System.out.println(hasil);
+        
+                
+    }
+}
---
+++ TUGAS PBO/PBO.java
@@ -0,0 +1,22 @@
+
+package pbo;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Variable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilai_pbo, nilai_web;
+        nilai_pbo = 99;
+        nilai_web = 27;
+        System.out.println("Variabel yang terdapat dalam program");
+        System.out.println("nilai_PBO = " + nilai_pbo);
+        System.out.println("nilai_web = " + nilai_web);
+    }
+    
+}
